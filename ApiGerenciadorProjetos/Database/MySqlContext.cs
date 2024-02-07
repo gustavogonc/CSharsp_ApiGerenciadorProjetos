5334834832f5cb2239de35abc3b2b1f345985c78
Gustavo Gonçalves <gustavoantonio01@hotmail.com>
2024-02-06 22:45:28 -0300

    Adicionado contexto do banco de dados na aplicação rodando no banco local

---
+++ ApiGerenciadorProjetos/Database/MySqlContext.cs
@@ -0,0 +1,19 @@
+﻿using ApiGerenciadorProjetos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiGerenciadorProjetos.Database
+{
+    public class MySqlContext : DbContext
+    {
+
+        public MySqlContext()
+        {
+            
+        }
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<User> User { get; set; }
+    }
+}
--- ApiGerenciadorProjetos/Program.cs
+++ ApiGerenciadorProjetos/Program.cs
@@ -1,3 +1,6 @@
+using ApiGerenciadorProjetos.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration["ConnectionStrings:MySql"]!;
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
 
